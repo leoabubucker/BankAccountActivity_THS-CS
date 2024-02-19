4609a3c96f678ff167c3c0b556bfac5c6254a96a
leoabubucker <leokumar@outlook.com>
2024-02-19 19:52:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+
+/* 
+
+Create a Program that creates a bank account, WRITE ALL CODE IN THE MAIN FUNCTION:
+
+1. Create variables that represent 
+  a) The bank's name
+  b) The Account Holder's Name
+  c) The Account Number
+  d) The Account Balance
+
+2. Print out the variables in the following way:
+  Bob's Bank of America account (ID: 204031) currently has $562.50 in it. 
+  Where Bob is the account holder name, Bank of America is the bank name, 204031 is the Account ID, and 562.50 is the account balance.
+
+3. Add $240.40 to the bank account
+
+4. Append a last name to the account name (i.e.; accountName is now Bob Smith)
+
+5. Increment the ID by 1
+
+6. Print the updated information in the same format described in 2). 
+*/
+
+class Main {
+  public static void main(String[] args) {
+    
+  }
+}
